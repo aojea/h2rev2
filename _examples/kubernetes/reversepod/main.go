785d99f6ba601311efff9c04ac741f854a0ad2ee
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2022-04-21 22:58:34 +0200

    use upgrade aware proxy

--- _examples/kubernetes/reversepod/main.go
+++ _examples/kubernetes/reversepod/main.go
@@ -8,39 +8,19 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/aojea/h2rev2"
 	"golang.org/x/net/http2"
+
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"k8s.io/klog/v2"
+	"k8s.io/kubectl/pkg/proxy"
 )
-
-// NewProxy takes target host and creates a reverse proxy
-func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
-	url, err := url.Parse(targetHost)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Printf("Reversing proxy to %s", url)
-	proxy := httputil.NewSingleHostReverseProxy(url)
-
-	return proxy, nil
-}
-
-// ProxyRequestHandler handles the http request using proxy
-func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("rev proxy rvc request %+v", r)
-		proxy.ServeHTTP(w, r)
-	}
-}
 
 func main() {
 	klog.InitFlags(nil)
@@ -80,16 +60,10 @@ func main() {
 		panic(fmt.Errorf("empty url"))
 	}
 
-	// initialize a reverse proxy and pass the actual backend server url here
-	proxy, err := NewProxy(config.Host)
+	server, err := proxy.NewServer("", "/", "", nil, config, 30*time.Second, false)
 	if err != nil {
 		panic(err)
 	}
-	transport, err := rest.TransportFor(config)
-	if err != nil {
-		panic(err)
-	}
-	proxy.Transport = transport
 
 	// kcp  -----> syncer (reverse connection)
 	client := &http.Client{}
@@ -123,11 +97,6 @@ func main() {
 		panic(err)
 	}
 	defer l.Close()
-	// serve requests
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", ProxyRequestHandler(proxy))
-	server := &http.Server{Handler: mux}
-	defer server.Close()
 	log.Printf("Serving on Reverse connection")
-	log.Fatal(server.Serve(l))
+	log.Fatal(server.ServeOnListener(l))
 }
